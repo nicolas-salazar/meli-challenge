dab2106ec5838c4107f1331f52c226c6583b3d5f
Nicolás Salazar <nicolas@ncodes.dev>
2021-05-09 11:18:50 -0500

    Created meli responses sample files for testing purposes

---
+++ backend/src/test-data/meli-responses-samples.js
@@ -0,0 +1,9 @@
+const MELI_DESCRIPTION_RESPONSE_SAMPLE = require('./description-sample.json');
+const MELI_DETAIL_RESPONSE_SAMPLE = require('./detail-sample.json');
+const MELI_SEARCH_RESPONSE_SAMPLE = require('./search-sample.json');
+
+module.exports = {
+  MELI_DESCRIPTION_RESPONSE_SAMPLE,
+  MELI_DETAIL_RESPONSE_SAMPLE,
+  MELI_SEARCH_RESPONSE_SAMPLE,
+};
